af65c240673a77253a804d574acabac1a67a2316
harshbagoria <139875633+harshbagoria@users.noreply.github.com>
2025-12-17 09:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+		
+		
+	}
+	get name(){
+		return name;
+}
+	get age(){
+		return age;
+	}
+	set age(age){
+		this.age = age;
+}
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+	
+	
+	}
+
+}
+
+class Student extends Person {
+		
+	study(){
+		console.log(`${name} is study`);
+		
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${name}is Teaching`);
+}
+}
 
 // Do not change the code below this line
 window.Person = Person;
